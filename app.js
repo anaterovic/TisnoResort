4b998aaac327504d9a5070ac4a163ee484898e71
Ana <terovic.ana@gmail.com>
2020-09-18 21:48:01 +0200

    Menu bar changes on hover with transitions and darker background--hopefully final for the main page

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-const logoHover = () => {
-    const logoPic = document.querySelector('.logo-menu');
-    const menuItems = document.querySelectorAll('.menu-item');
-    const menu = document.querySelector('div.menu');
+$(function(){
+    const logoHover = () => {
+        const logoPicBig = document.querySelector('.logo-menu-resort-big');
+        const logoPicSmall = document.querySelector('.logo-menu-resort-small');
+        const menuItems = document.querySelectorAll('.menu-item');
+        const menu = document.querySelector('.navigation');
+        const bg = document.querySelector('.background');
+        const bk2 = document.querySelector('.logo-menu-booking');
+        const bk = document.querySelectorAll('.slideshow li span');
 
-    menu.addEventListener("mouseover", ()=>{
-        console.log("Tu sam.")
-        menuItems.forEach((link,index)=>{
-            link.style.display = "block";
-        })
-        logoPic.src = "images/logo-new-bolder.png";  
-        logoPic.style.width = "100px";
-        logoPic.style.height = "50px";
-    })
+        menu.addEventListener("mouseover", ()=>{
+            console.log("Tu sam.")
+            $('.background').slideDown(300);
+            $('.logo-menu-resort-big').hide("fast")
+            logoPicSmall.style.display = "flex";
+            menuItems.forEach((link,index)=>{
+                link.style.display= "inline-flex";
+                link.style.animation = `titleAnimation 0.5s ease forwards`;
+                // ${index/5+0.3}s `;
+            });
+            bk.forEach((pic,index)=>{
+                pic.style.filter = "brightness(50%)";
+            })
+        });
+        
+        menu.addEventListener("mouseleave", ()=>{
+            console.log("Idem.");
+            menuItems.forEach((link,index)=>{
+                link.style.display = "none";
+            })
+            $('.logo-menu-resort-small').hide("fast")
+            $('.background').slideUp(300);
+            $('.logo-menu-resort-big').show("fast");
+            bk.forEach((pic,index)=>{
+                pic.style.filter = "brightness(100%)";
+            })
+         })
+    }
     
-    menu.addEventListener("mouseout", ()=>{
-        console.log("Idem.")
-        menuItems.forEach((link,index)=>{
-            link.style.display = "none";
-        })
-        logoPic.src = "images/logo-new-long.png";
-        logoPic.style.width = "240px";
-        logoPic.style.height = "28px";
-    })
-
-}
-
-logoHover();
+    logoHover();
+});
